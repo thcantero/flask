737868ef5621bbcb5b6bafc657ece200be56003b
Thalia Cantero <46179477+thcantero@users.noreply.github.com>
2024-06-30 23:35:25 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,57 +1,15 @@
 from flask import Flask, request
-from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
-"""Basic operations"""
+@app.route('/welcome')
+def welcome():
+    return 'welcome'
 
-@app.route('/add')
-def do_add():
-    """Add a + b parameters"""
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = add(a,b)
-    return str(result)
+@app.route('/welcome/home')
+def welcome_home():
+    return 'welcome home'
 
-@app.route('/sub')
-def do_sub():
-    """Substract a - b param"""
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = sub(a,b)
-    return str(result)
-
-@app.route('/mult')
-def do_mult():
-    """Multiply a and b param"""
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = mult(a,b)
-    return str(result)
-
-@app.route('/div')
-def do_div():
-    """Divide a by b param"""
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = div(a,b)
-    return str(result)
-
-###Further Study
-
-operations = {
-    'add': add,
-    'sub': sub,
-    'mult': mult,
-    'div': div,
-    }
-
-@app.route('/math/<op>')
-def do_math(op):
-    'Do operation with a & b'
-
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-
-    return str(operations[op](a,b))
-    
+@app.route('/welcome/back')
+def welcome_back():
+    return 'welcome back'
--- test.py
+++ test.py
@@ -1,43 +1,22 @@
 from app import app
 import unittest
 
-class TestCalculator(unittest.TestCase):
-    def setUp(self):
-        self.client = app.test_client(self)
+class TestSimpleRoutes(unittest.TestCase):
 
-    def test_add(self):
-        response = self.client.get('/add?a=2&b=2')
-        self.assertIn(b'4', response.data)
+    def test_welcome(self):
+        tester = app.test_client(self)
+        response = tester.get('/welcome', content_type='html/text')
+        self.assertIn(b'welcome', response.data.lower())
         self.assertEqual(response.status_code, 200)
 
-    def test_subtract(self):
-        response = self.client.get('/sub?a=2&b=2')
-        self.assertIn(b'0', response.data)
+    def test_welcome_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/welcome/home', content_type='html/text')
+        self.assertIn(b'welcome home', response.data.lower())
         self.assertEqual(response.status_code, 200)
 
-    def test_multiply(self):
-        response = self.client.get('/mult?a=20&b=2')
-        self.assertIn(b'40', response.data)
-        self.assertEqual(response.status_code, 200)
-
-    def test_division(self):
-        response = self.client.get('/div?a=2&b=2')
-        self.assertIn(b'1', response.data)
-        self.assertEqual(response.status_code, 200)
-
-    def test_all_in_one(self):
-        response = self.client.get('/math/add?a=2&b=2')
-        self.assertIn(b'4', response.data)
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.get('/math/sub?a=2&b=2')
-        self.assertIn(b'0', response.data)
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.get('/math/mult?a=20&b=2')
-        self.assertIn(b'40', response.data)
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.get('/math/div?a=2&b=2')
-        self.assertIn(b'1', response.data)
+    def test_welcome_back(self):
+        tester = app.test_client(self)
+        response = tester.get('/welcome/back', content_type='html/text')
+        self.assertIn(b'welcome back', response.data.lower())
         self.assertEqual(response.status_code, 200)
